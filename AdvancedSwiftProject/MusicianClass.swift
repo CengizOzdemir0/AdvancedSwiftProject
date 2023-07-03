18019025b7a2486808950f2165ced2ee52a3656a
cozdemir <cengiz.ozdemir@assistt.com.tr>
2023-07-03 11:05:47 +0300

    MusicianClass create

---
+++ AdvancedSwiftProject/MusicianClass.swift
@@ -0,0 +1,21 @@
+//
+//  MusicianClass.swift
+//  AdvancedSwiftProject
+//
+//  Created by Cengiz ÖZDEMİR on 3.07.2023.
+//
+
+import Foundation
+
+class MusicianClass {
+    var name : String
+    var age : Int
+    var instrument : String
+    
+    init(nameInput: String, ageInput: Int, instrumentInput: String) {
+        self.name = nameInput
+        self.age = ageInput
+        self.instrument = instrumentInput
+    }
+    
+}
---
+++ AdvancedSwiftProject/MusicianStruct.swift
@@ -0,0 +1,8 @@
+//
+//  MusicianStruct.swift
+//  AdvancedSwiftProject
+//
+//  Created by Cengiz ÖZDEMİR on 3.07.2023.
+//
+
+import Foundation
