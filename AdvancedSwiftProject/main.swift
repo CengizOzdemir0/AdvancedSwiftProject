1969f2f1161957c565952a928eac90ca1a9fde75
cozdemir <cengiz.ozdemir@assistt.com.tr>
2023-07-03 14:37:00 +0300

    Guard

--- AdvancedSwiftProject/main.swift
+++ AdvancedSwiftProject/main.swift
@@ -53,23 +53,43 @@ structJames.happyBirthday()
 // TUPLE
 // Birden fazla veriyi aynı kolesksiyon içinde göstermek için, tam anlamıyla dizi değil
 let myTuple = (1,3)
-print(myTuple.0)
+//print(myTuple.0)
 
 var myTuple2 = (2,4,6)
-print(myTuple2.2)
+//print(myTuple2.2)
 myTuple2.2 = 10
-print(myTuple2.2)
+//print(myTuple2.2)
 
 let myTuple3 = ("Jake",100)
 
 let myTuple4 = (10,[10,20,30])
-print(myTuple4.1[1])
+//print(myTuple4.1[1])
 
 let predefinedTuple : (String, String)
 predefinedTuple.0 = "Jake"
 predefinedTuple.1 = "Sam"
-print(predefinedTuple)
+//print(predefinedTuple)
 
 let newTuple = (name:"James", metallica: true)
-print(newTuple.metallica)
-print(newTuple.name)
+//print(newTuple.metallica)
+//print(newTuple.name)
+
+// Guard Let vs if let
+// guard let biraz daha katı, koruma güdüsü var
+
+let myNumber = "5"
+func converToIntegerGuard (stringInput : String) -> Int {
+    guard let myInteger = Int(stringInput) else {
+        return 0
+    }
+    return myInteger
+}
+func convertToIntegerIf (stringInput : String) -> Int {
+    if let myInteger = Int(stringInput) {
+        return myInteger
+    } else {
+        return 0
+    }
+}
+print(converToIntegerGuard(stringInput: myNumber))
+print(convertToIntegerIf(stringInput: myNumber))
